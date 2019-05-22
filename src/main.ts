a4ecf883c95dccb770c642d09ceb8ff1f90746d8
dkipp <mail@daniel-kipp.de>
2019-05-22 22:21:00 +0200

    initial

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { defineCustomElements } from '@orgenic/orgenic-ui/dist/loader';
+
 Vue.config.productionTip = false;
+Vue.config.ignoredElements = [/og-\w*/];
+
+defineCustomElements(window);
 
 new Vue({
   router,
---
+++ src/views/Patienten.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="patienten">
+    <h1>Demo-Patienten anlegen</h1>
+
+    <og-form-item label="Datenverzeichnis">
+        <og-text-input :value="value" v-on:valueChanged="onTextChanged" placeholder=""></og-text-input>
+    </og-form-item>
+
+    <og-form-item label="Anzahl">
+        <og-number-input min="100" max="5000" step="100" value="2500"></og-number-input>
+    </og-form-item>
+
+
+    <og-button label="generate" data-context="workflow" clicked="onClicked"></og-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Patienten extends Vue {
+  @Prop() private value!: string;
+
+  public onTextChanged() {
+    //
+  }
+}
+</script>
